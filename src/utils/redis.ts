7dc74f89a6d11850bd4c496b7f4662da4ae90cec
kevinonyoh <evionyoh@gmail.com>
2025-02-15 11:16:03 +0100

    first commit

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,12 +1,12 @@
 import Redis from "ioredis";
 
-// const redis = new Redis({
-//     host: process.env.REDIS_HOST,
-//     port: process.env.REDIS_PORT,
-//     password: process.env.REDIS_PASSWORD
-// });
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD
+});
 
-const redis = new Redis();
+// const redis = new Redis();
 
 
 const redisSet = async ( email: string, token: string ) => {
